7ea321cae2010e511640f44b9d1f8c706cb0500c
NeonJarbas <59943014+NeonJarbas@users.noreply.github.com>
2022-06-15 20:25:57 +0100

    add license tests and workflow (#15)
    * add license tests and workflow
    
    * Update license tests
    
    * Remove unidecode dependency
    Update license tests
    
    * Fix bug in clean_voice_name
    Add neon-api-proxy license to whitelist
    
    * Troubleshooting test failures
    
    * Troubleshooting unit test failures
    
    * Add setuptools license override
    
    Co-authored-by: jarbasai <jarbasai@mailfence.com>
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- neon_tts_plugin_polly/__init__.py
+++ neon_tts_plugin_polly/__init__.py
@@ -24,7 +24,6 @@ from neon_utils.logger import LOG
 from neon_utils.parse_utils import format_speak_tags
 from ovos_plugin_manager.templates.tts import TTS, TTSValidator
 from ovos_utils.metrics import Stopwatch
-from unidecode import unidecode
 
 from neon_tts_plugin_polly.util import get_credentials_from_file
 
@@ -120,10 +119,17 @@ class PollyTTS(TTS):
                 elif "Joey" in voices:
                     sel_voice = "Joey"
                 else:
-                    sel_voice = unidecode(voices[0])
+                    sel_voice = self._clean_voice_name(voices[0])
             self._voice_cache[cache_key] = sel_voice
         LOG.debug(f"Get Voice time={stopwatch.time}")
         return sel_voice
+
+    @staticmethod
+    def _clean_voice_name(voice):
+        import unicodedata
+        return unicodedata.normalize('NFKD',
+                                     voice).encode('ASCII',
+                                                   'ignore').decode('ASCII')
 
 
 class PollyTTSValidator(TTSValidator):
